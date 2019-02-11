3dd59f38062b7a62211256b1104cd12a713e67ca
abnerord <abner.ord@gmail.com>
2019-02-10 23:44:47 -0600

    Programa Finalizado
    Los comentarios estan realizados, y el programa funcional

--- codBase/js/app.js
+++ codBase/js/app.js
@@ -1,32 +1,41 @@
+ 
+ //Funcion calculadora, donde se engloba todo el proyecto usando el prototipo Modelo.
  var calculadora = (function(){
 
-    var display = document.getElementById("display");
+//Declaracion de las variables utilizadas en el programa
+
+    var display = document.getElementById("display"); //Toma el valor del id display, el cual representa la pantalla de la computadora
     var punto = false;
     var negativo = false;
     var secuencia = true;
     var operacion,limite,subcadena;
     var numero1 = 0, numero2 = 0, resultado=0;
 
+//area de funciones del programa
+//funciones que corresponden a los eventos click de cada imagen del html 
+//existen dos funciones por cada imagen, la funcion down, para cunado se oprime, y la opcion up que representa cuadno se deja de oprimir la imgane.
+
     function OnDown(){
-        document.getElementById('on').style=" width: 20%;";
-        display.innerHTML = "0";
-        punto=false;
-        negativo=false;
-        secuencia=false;
-        numero1=0;
+        document.getElementById('on').style=" width: 20%;"; //modifica el damaño del boton
+        display.innerHTML = "0"; //regresa el valor a 0 del display
+         punto=false;   // regresa a false el valor que se utiliza para verificar la existencia del punto decimal
+        negativo=false; //regresa a false el valor que se utiliza para verificar si un numero es negativo o positivo
+        secuencia=false; //regresa a false el valor que se utiliza para guardar el ultimo valor y la ultima operacion, para una operacion en secuencia 
+        numero1=0; //devuelvo los valores por default
         numero2=0;
         setTimeout(OnUp,100);
     };
 
-    
+//Las funciones UP devuelver el valor original a la imgen     
     function OnUp(){
         document.getElementById('on').style=" width: 22%;";
     };
 
+// Inicio de las funciones aritmeticas con transpaso de parametros
     function suma (num1,num2){
         resultado = num1 + num2;
         limite = resultado.toString();
-        if (limite.length > 8){
+        if (limite.length > 8){          //estos if sirven para que los digitos no sobre pasen el valor de 8 digitos por numero mostrado en el display
 
             subcadena = limite.substring(0,7);
             display.innerHTM = subcadena;
@@ -40,7 +49,7 @@
     function resta (num1,num2){
         resultado = num1 - num2;
         limite = resultado.toString()
-        if (limite.length > 8){
+        if (limite.length > 8){              //estos if sirven para que los digitos no sobre pasen el valor de 8 digitos por numero mostrado en el display
 
             subcadena = limite.substring(0,7);
             display.innerHTM = subcadena;
@@ -54,7 +63,7 @@
     function division (num1,num2){
         resultado = num1 / num2;
         limite = resultado.toString()
-        if (limite.length > 8){
+        if (limite.length > 8){                      //estos if sirven para que los digitos no sobre pasen el valor de 8 digitos por numero mostrado en el display
 
             subcadena = limite.substring(0,8);
             display.innerHTM = subcadena;
@@ -68,7 +77,7 @@
     function multiplicacion (num1,num2){
         resultado = num1 * num2;
         limite = resultado.toString()
-        if (limite.length > 8){
+        if (limite.length > 8){                 //estos if sirven para que los digitos no sobre pasen el valor de 8 digitos por numero mostrado en el display
 
             subcadena = limite.substring(0,7);
             display.innerHTM = subcadena;
@@ -78,10 +87,11 @@
         
         display.innerHTML= subcadena;
     }
-
+//final de las funciones aritmeticas
+//inicio de las funciones por tecla 
 
     function signDown(){
-        document.getElementById('sign').style=" width: 20%;";
+        document.getElementById('sign').style=" width: 20%;";  //Funcion que modifica el signo a la cantidad del display 
         
         if(negativo==false && display.innerHTML!= 0){
             display.innerHTML = "-"+ display.innerHTML;
@@ -98,7 +108,7 @@
             document.getElementById('sign').style=" width: 22%;";
     };
 
-    function raizDown(){
+    function raizDown(){                                            //Funcion que cambia el tamaño de la imagen raiz cuadrada
         document.getElementById('raiz').style=" width: 20%;";
         setTimeout(raizUp,100);
     };
@@ -107,7 +117,7 @@
             document.getElementById('raiz').style=" width: 22%;";
     };
 
-    function divididoDown(){
+    function divididoDown(){                                            //funcion que activa la operacion de division 
         document.getElementById('dividido').style=" width: 20%;";
         numero1= Number(display.innerHTML);
         display.innerHTML = "";
@@ -123,7 +133,7 @@
     };
 
     function sieteDown(){
-
+                                                                        //funcin del numero siete, agrega su valor al display 
         document.getElementById('7').style=" width: 20%;";
         if (display.innerHTML == "0"){
            
@@ -142,7 +152,7 @@
             document.getElementById('7').style=" width: 22%;";
     };
 
-    function seisDown(){
+    function seisDown(){                                            //funcin del numero seis, agrega su valor al display 
         document.getElementById('6').style=" width: 20%;";
         if (display.innerHTML == "0"){
             display.innerHTML = "6";
@@ -156,7 +166,7 @@
             document.getElementById('6').style=" width: 22%;";
     };
 
-    function ochoDown(){
+    function ochoDown(){                                             //funcin del numero ocho, agrega su valor al display 
         document.getElementById('8').style=" width: 20%;";
         if (display.innerHTML == "0"){
             display.innerHTML = "8";
@@ -170,7 +180,7 @@
             document.getElementById('8').style=" width: 22%;";
     };
 
-    function nueveDown(){
+    function nueveDown(){                                           //funcin del numero nueve, agrega su valor al display 
         document.getElementById('9').style=" width: 20%;";
         if (display.innerHTML == "0"){
             display.innerHTML = "9";
@@ -184,7 +194,7 @@
             document.getElementById('9').style=" width: 22%;";
     };
 
-    function porDown(){
+    function porDown(){                                             //Funcion para efector la operacion Multiplicacion 
         document.getElementById('por').style=" width: 20%;";
         numero1= Number(display.innerHTML);
         display.innerHTML = "";
@@ -198,7 +208,7 @@
     function porUp(){
             document.getElementById('por').style=" width: 22%;";
     };
-
+//funcin del numero cuatro, agrega su valor al display 
     function cuatroDown(){
         document.getElementById('4').style=" width: 20%;";
         if (display.innerHTML == "0"){
@@ -212,7 +222,7 @@
     function cuatroUp(){
             document.getElementById('4').style=" width: 22%;";
     };
-
+    //funcin del numero cinco, agrega su valor al display 
     function cincoDown(){
         document.getElementById('5').style=" width: 20%;";
         if (display.innerHTML == "0"){
@@ -226,7 +236,7 @@
     function cincoUp(){
             document.getElementById('5').style=" width: 22%;";
     };
-
+//Funcion que agrega la operacion restar 
     function menosDown(){
         document.getElementById('menos').style=" width: 20%;";
         setTimeout(menosUp,100);
@@ -241,7 +251,7 @@
     function menosUp(){
             document.getElementById('menos').style=" width: 22%;";
     };
-
+//funcin del numero uno, agrega su valor al display 
     function unoDown(){
         document.getElementById('1').style=" width: 27%;";
         if (display.innerHTML == "0"){
@@ -255,7 +265,7 @@
     function unoUp(){
             document.getElementById('1').style=" width: 30%;";
     };
-
+//funcin del numero dos, agrega su valor al display 
     function dosDown(){
         document.getElementById('2').style=" width: 27%;";
         if (display.innerHTML == "0"){
@@ -269,7 +279,7 @@
     function dosUp(){
             document.getElementById('2').style=" width: 30%;";
     };
-
+//funcin del numero tres, agrega su valor al display 
     function tresDown(){
         document.getElementById('3').style=" width: 27%;";
         if (display.innerHTML == "0"){
@@ -283,6 +293,7 @@
     function tresUp(){
             document.getElementById('3').style=" width: 30%;";
     };
+//funcin del numero cero, agrega su valor al display 
     function ceroDown(){
         document.getElementById('0').style=" width: 27%;";
         if (display.innerHTML == "0"){
@@ -296,7 +307,7 @@
     function ceroUp(){
             document.getElementById('0').style=" width: 30%;";
     };
-
+//Funcion igual, llama a la funcion correspondiende a cada operacion y agrega el segundo numero a dicha operacion 
     function igualDown(){
         document.getElementById('igual').style=" width: 27%;";
         
@@ -349,7 +360,7 @@
     function igualUp(){
             document.getElementById('igual').style=" width: 30%;";
     };
-
+//Funcion mas
     function masDown(){
         document.getElementById('mas').style=" height: 90%;";
        
@@ -380,7 +391,7 @@
             document.getElementById('punto').style=" width: 30%;";
     };
 
-
+//Eventos que corresponden a los clicks de cada imagen del HTML
     document.getElementById('on').onclick = OnDown;
     document.getElementById('sign').onclick = signDown;
     document.getElementById('raiz').onclick = raizDown;
@@ -403,4 +414,4 @@
 
 })(); 
 
- 
+ //Final de la funcion modul calculadora
